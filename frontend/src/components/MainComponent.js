0101c18c62ee7efce7c8710895c63a18d896086b
Sameera <Sameeranicho@gmail.com>
2021-10-12 20:10:30 +0530

    packagesadded

--- frontend/src/components/MainComponent.js
+++ frontend/src/components/MainComponent.js
@@ -3,13 +3,12 @@ import Home from "./HomeComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Menu from "./MenuComponent";
-<<<<<<< HEAD
+
 import StudentCom from "./StudentCom";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-=======
+
 import StudentList from "./StudentComponent";
-import { Switch, Route, Redirect,useParams  } from "react-router-dom";
->>>>>>> 52e1060bdf9124b89dceabe0aa22dc69a6d1ee14
+
 import { ITEMS } from "../shared/functionItems";
 import { STUDENTS } from "../shared/studentList";
 //Notice M - Imports
@@ -46,7 +45,7 @@ class Main extends Component {
           <Route path="/" exact component={() => <Redirect to="/home" />} />
 
           <Route path="/home" component={HomePage} />
-<<<<<<< HEAD
+
 
           <Route exact path="/menu" component={() => <Menu items={this.state.items} />}/>  
 
@@ -54,7 +53,7 @@ class Main extends Component {
 
           <Route path="/students" exact component={() => <Redirect to="/students"/>}><StudentCom /></Route>
                      
-=======
+
           <Route
             exact
             path="/menu"
@@ -101,7 +100,6 @@ class Main extends Component {
             component={() => <Report/>}
           />
       
->>>>>>> 52e1060bdf9124b89dceabe0aa22dc69a6d1ee14
           
           <Redirect to="/home" />
         </Switch>
